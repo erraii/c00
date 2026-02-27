be9749b04f0b01631a8a8c32f58edd9fb9197daa
erraii <eraycakiray@yahoo.com>
2026-02-27 11:47:53 +0100

    Add files via upload

---
+++ ex01/ft_print_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecakiray <ecakiray@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/26 12:18:13 by ecakiray          #+#    #+#             */
+/*   Updated: 2026/02/26 12:39:07 by ecakiray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		write(1, &ch, 1);
+		ch++;
+	}
+}
+
+//int	main(void)
+//{
+//	ft_print_alphabet();
+//	return (0);
+//}	
---
+++ ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecakiray <ecakiray@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/26 12:28:34 by ecakiray          #+#    #+#             */
+/*   Updated: 2026/02/26 12:40:29 by ecakiray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	ch;
+
+	ch = 'z';
+	while (ch >= 'a')
+	{
+		write(1, &ch, 1);
+		ch--;
+	}
+}
+
+//int	main(void)
+//{
+//	ft_print_reverse_alphabet();
+//	return (0);
+//}	
